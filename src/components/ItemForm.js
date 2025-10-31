2ffe870e51f3359892ff9b82b2c9eb805329209c
Kollyns12 <collinsmoses295@gmail.com>
2025-10-31 09:06:22 +0300

    adds my updated files

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ onAddItem }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {
+      name: name,
+      category: category,
+      isInCart: false,
+    };
+
+    fetch("http://localhost:4000/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem),
+    })
+      .then((r) => r.json())
+      .then((addedItem) => {
+        onAddItem(addedItem);
+        setName("");
+        setCategory("Produce");
+      });
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
@@ -35,3 +59,5 @@ function ItemForm() {
 }
 
 export default ItemForm;
+
+
